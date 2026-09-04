66df2460f15d74fb9c51cdf54eeb6e8a6bd10cab
ajaymalli2231-design <ajaymalli2231@gmail.com>
2026-09-04 22:57:03 +0530

    Initialize Firebase in main function

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const LaxmiTradingApp());
 }
+
 
 class LaxmiTradingApp extends StatelessWidget {
   const LaxmiTradingApp({super.key});
